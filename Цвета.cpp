2fc1ea87d36deacc38f811f3633f66cba6d243f0
georgij-gamma <georgij-gamma@mail.ru>
2021-02-19 22:42:12 +1000

    19.02.2021#2

--- Цвета.cpp
+++ Цвета.cpp
@@ -86,3 +86,41 @@ foreground = 1111;
 // И наконец, завершающей операцией является явное приведение через c-style cast к двухбайтовому типу WORD.
 (WORD) 1001 1111 = 0000 0000 1001 1111 // (в big-endian)
 1001 1111 0000 0000 // (в little-endian).
+///////////////////////////////////////////////////////////
+// FOR LINUX:
+#ifndef _COLORMODE_H_
+#define _COLORMODE_H_
+#include <ostream>
+namespace Color {
+    enum Code {
+        FG_RED      = 31,
+        FG_GREEN    = 32,
+        FG_BLUE     = 34,
+        FG_DEFAULT  = 39,
+        BG_RED      = 41,
+        BG_GREEN    = 42,
+        BG_BLUE     = 44,
+        BG_DEFAULT  = 49
+    };
+    class Modifier {
+        Code code;
+    public:
+        Modifier(Code pCode) : code(pCode) {}
+        friend std::ostream&
+        operator << (std::ostream& os, const Modifier& mod) {
+            return os << "\033[" << mod.code << "m";
+        }
+    };
+}
+#endif
+
+// using:
+#include "colormod.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+    Color::Modifier red(Color::FG_RED);
+    Color::Modifier def(Color::FG_DEFAULT);
+    cout << "This ->" << red << "word" << def << "<- is red." << endl;
+}
