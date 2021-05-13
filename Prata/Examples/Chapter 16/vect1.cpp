5818fed76229285f307675e409c42492895e7694
georgij-gamma <georgij-gamma@mail.ru>
2021-05-14 00:14:54 +1000

    14.05.2021#1

--- Prata/Examples/Chapter 16/vect1.cpp
+++ Prata/Examples/Chapter 16/vect1.cpp
@@ -1,37 +1,30 @@
-// vect1.cpp -- introducing the vector template
+// vect1.cpp -- пример работы с шаблоном vector
 #include <iostream>
 #include <string>
 #include <vector>
 
-const int NUM = 5;
+const int NUM = 2;
 int main()
 {
-    using std::vector;
-    using std::string;
-    using std::cin;
-    using std::cout;
-    using std::endl;
-
-    vector<int> ratings(NUM);
-    vector<string> titles(NUM);
-    cout << "You will do exactly as told. You will enter\n"
-         << NUM << " book titles and your ratings (0-10).\n";
+    std::vector<int> ratings(NUM);
+    std::vector<std::string> titles(NUM);
+    std::cout << "You will do exactly as told. You will enter\n"
+              << NUM << " book titles and your ratings (0-10).\n";
     int i;
     for (i = 0; i < NUM; i++)
     {
-        cout << "Enter title #" << i + 1 << ": ";
-        getline(cin,titles[i]);
-        cout << "Enter your rating (0-10): ";
-        cin >> ratings[i];
-        cin.get();
+        std::cout << "Enter title #" << i + 1 << ": ";
+        std::getline(std::cin, titles[i]);
+        std::cout << "Enter your rating (0-10): ";
+        std::cin >> ratings[i];
+        std::cin.get();
     }
-    cout << "Thank you. You entered the following:\n"
-          << "Rating\tBook\n";
+    std::cout << "Thank you. You entered the following:\n"
+              << "Rating\tBook\n";
     for (i = 0; i < NUM; i++)
     {
-        cout << ratings[i] << "\t" << titles[i] << endl;
+        std::cout << ratings[i] << "\t"
+                  << titles[i] << '\n';
     }
-    // cin.get();
-
     return 0; 
 }
